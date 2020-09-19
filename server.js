20ea072e1d5d3e7092cc9daed4387c3e168c6f19
Evangelos Kolimitras <evangeloskolimitras@outlook.com>
2020-09-19 21:24:55 +0200

    favicon added

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ require('./connection/connection');
 const app = express();
 const routes = require("./routes/routes.js")
 const portListener = require("./utils/portListener");
+const favicon = require('serve-favicon')
+const path = require('path');
 
+app
+	.use(favicon(path.join(__dirname, 'views', 'favicon.ico')))
 app
 	.use(express.static(__dirname + '/views'));
 app
@@ -12,5 +16,6 @@ app
 	.use(express.urlencoded({ extended: false }));
 app
 	.use(routes)
+
 app
 	.listen(...portListener(process.env.PORT));
